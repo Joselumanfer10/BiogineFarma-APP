5c46327e555b2f5d52d2d34b7410d8719d16d1c6
Joselumanfer10 <148992377+Joselumanfer10@users.noreply.github.com>
2023-12-14 13:49:53 +0100

    Add files via upload

--- APP2.py
+++ APP2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# In[ ]:
 
 
 import streamlit as st
@@ -20,39 +20,37 @@ st.title("BIOGINEFARMA")  # Usa st.title para un texto más grande
 # Título de la aplicación
 st.title("Búsqueda de producto por número de lote")
 
-# Pedir al usuario que introduzca el número de lote del producto
-numero_lote_buscar = st.text_input("INTRODUCE EL NÚMERO DE LOTE DEL PRODUCTO EN MAYÚSCULAS:")
+# Autocompletado para el número de lote
+numero_lote_buscar = st.selectbox("SELECCIONA O ESCRIBE EL NÚMERO DE LOTE DEL PRODUCTO:",
+                                   df['LOTE'].unique(),
+                                   format_func=lambda x: f"{x}")  # Muestra las sugerencias con formato
 
-# Verificar si se ingresó un número de lote
+# Verificar si se seleccionó un número de lote
 if numero_lote_buscar:
     # Realizar la búsqueda basada en el número de lote
     resultado = df.loc[df['LOTE'] == numero_lote_buscar]
 
     if resultado.empty:
-        st.warning("El número de lote no fue encontrado.")
+        st.warning("El número de lote no fue encontrado. Recuerda que si lleva letras deben estar en mayúsculas")
     else:
         producto = resultado.iloc[0]['PRODUCTO']
         calle = int(resultado.iloc[0]['CALLE'])
         jaula = int(resultado.iloc[0]['JAULA'])
         cajon = int(resultado.iloc[0]['CAJON'])
         stock = int(resultado.iloc[0]['STOCK'])
+        
         # Mostrar la información en diferentes párrafos
         st.success(f"El producto {producto} con número de lote {numero_lote_buscar} se encuentra en:")
         st.write(f"Calle: {calle}")
         st.write(f"Jaula: {jaula}")
         st.write(f"Cajón: {cajon}")
         st.write(f"Stock: {stock} pcs.")
-        
-        enlace_imagen = resultado.iloc[0]['PNG']
-        st.image(enlace_imagen, caption=f"Imagen del producto {producto}", use_column_width=True)
 
         # Mostrar plano del almacen
         foto_almacen = "PLANO.png"
         st.image(foto_almacen, caption="Plano del almacén", use_column_width=True)
-
-
-# In[ ]:
-
-
-
+        
+        # Mostrar imagen producto
+        enlace_imagen = resultado.iloc[0]['PNG']
+        st.image(enlace_imagen, caption=f"Imagen del producto {producto}", use_column_width=True)
 
